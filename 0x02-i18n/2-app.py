b3543788129711e79e74bb2e14a8c830630f185f
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-02-09 03:43:05 +0300

    locale selector

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Babel setup"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -19,10 +19,16 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    """get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', strict_slashes=False)
 def index():
     """default route"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
